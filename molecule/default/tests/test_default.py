99e95ff9f097467cefb3a8770d98a18a433889c5
Teodoro Cook <teodoro.cook@gmail.com>
2017-11-12 14:56:32 -0600

    Fix gitlab_edition variable interpolation error

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,5 +6,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_command(Command):
-    assert Command('gitlab --version').rc == 0
+def test_command(Host):
+    assert Host.command('gitlab-ctl reconfigure').rc == 0
+    assert Host.command('gitlab-ctl restart').rc == 0
+    assert Host.command('gitlab-ctl status').rc == 0
+
+def test_socket(Host):
+    assert Host.socket('tcp://0.0.0.0:80').is_listening
